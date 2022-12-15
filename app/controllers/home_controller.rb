46fe5ebb5ea23141d01cb8155953789aa5e956f8
zairlaka <zair.100.pro@gmail.com>
2022-12-15 23:55:16 +0500

    update logic for essential docs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
     def index
-        @ud = current_user.user_docs.where('extract(year  from created_at) >= ?', 2022)
+        if current_user.role == "User"
+            @ud = current_user.user_docs.where('extract(year  from created_at) >= ?', 2022)
+            @req_dp = current_user.profile_image.attached? ?  current_user.profile_image.created_at.year >= 2022 ? true : false : tru
+            @req_doc = ["Drivers License", "Insurance card"]
+            current_user.user_docs.each { |t| @req_doc.delete(t.document_type.name) unless @req_doc.include?(t.document_type.name) }
+        end
     end
 end
