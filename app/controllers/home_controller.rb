ec2820520b12ded017da1d6c8cc4053a6d7714fa
zairlaka <zair.100.pro@gmail.com>
2023-01-07 23:43:34 +0500

    fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
     def index
         if current_user.role == "User"
             @ud = current_user.user_docs.where('extract(year  from created_at) >= ?', 2022)
-            @req_dp = current_user.profile_image.attached? ?  current_user.profile_image.created_at.year >= 2022 ? true : false : tru
+            @req_dp = current_user.profile_image.attached? ?  current_user.profile_image.created_at.year >= 2022 ? true : false : true
             @req_doc = ["Drivers License", "Insurance card"]
             current_user.user_docs.each { |t| @req_doc.delete(t.document_type.name) unless @req_doc.include?(t.document_type.name) }
         end
