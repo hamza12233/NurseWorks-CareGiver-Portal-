e6bbba762601e47bb85c3b67e434808ce2e68476
hamza <hafizhamzasaleem4@gmail.com>
2023-07-01 08:08:50 +0500

    fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,7 +104,7 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
 config.action_mailer.default_url_options = { host: "https://app.caremountain.com" }
 
-ActionMailer::Base.smtp_settings = {
+config.action_mailer.smtp_settings = {
   domain:         'app.caremountain.com',
   address:        "smtp.sendgrid.net",
   port:            587,
