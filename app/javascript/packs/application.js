bd408bff905b2bf9c3f51ba5de17a182ab7419da
hamza <hafizhamzasaleem4@gmail.com>
2024-03-25 09:22:54 +0500

    Revert "packages updated"
    This reverts commit 2c1471f1a031afa91d3a308376c7d6cd34e79c8a.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,17 +3,19 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs";
-import Turbolinks from "turbolinks";
-import * as ActiveStorage from "@rails/activestorage";
-import "channels";
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
 
-Rails.start();
-Turbolinks.start();
-ActiveStorage.start();
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
 
-require("bootstrap");
-require("bootstrap-datepicker");
+
+require("bootstrap")
+require("bootstrap-datepicker")
+
 
 //= require jquery-3.2.1.slim.min
 //= require jquery_ujs
@@ -26,14 +28,14 @@ require("bootstrap-datepicker");
 //= require turbolinks
 //= require_tree .
 
-import "../stylesheets/application";
 
+import "../stylesheets/application";
 document.addEventListener("turbolinks:load", function() {
   $(function () {
       $('[data-toggle="tooltip"]').tooltip();
       $('[data-toggle="popover"]').popover();
-  });
-});
+  })
+})
 
 document.addEventListener("turbolinks:load", function() {
   $(function () {
@@ -43,7 +45,7 @@ document.addEventListener("turbolinks:load", function() {
       $.ajax({
         url: "users/closed_without_docs"
       });
-    });
+    })
 
     $("body").on('click', '.toggle-password', function() {
       $(this).toggleClass("fa-eye fa-eye-slash");
@@ -53,17 +55,20 @@ document.addEventListener("turbolinks:load", function() {
       } else {
         input.attr("type", "password");
       }
+    
     });
-  });
+  })
 
   $(function(){
     $('.datepicker').datepicker({
       format: 'mm-dd-yyyy'
     });
   });
-});
 
-require("trix");
-require("@rails/actiontext");
+})
 
-Turbolinks.setProgressBarDelay(1000);
+require("trix")
+require("@rails/actiontext")
+
+Turbolinks.setProgressBarDelay(1000)
+
